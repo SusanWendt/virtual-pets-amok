e7dcbcfdf920652cc7dce65124584fa078d9fc48
SusanWendt <wendt.37@osu.edu>
2018-02-11 23:02:08 -0500

    happiness and health fixed

--- src/main/java/virtualpetsamok/VirtualPet.java
+++ src/main/java/virtualpetsamok/VirtualPet.java
@@ -5,8 +5,6 @@ public abstract class VirtualPet {
 	// protected String status for abstract class;
 	protected String name;
 	protected String description;
-	protected String happiness; 
-	protected int health;
 
 	// getters
 	public String getName() {
@@ -17,31 +15,20 @@ public abstract class VirtualPet {
 		return description;
 	}
 	
-	public int getHealth() {
-		return health;
-	}
-	
-	public String getHappiness() {
-		return happiness;
-	}
-
 	protected abstract String health();
 
 	public String happiness() {
 		if (health().equals("Dead") || health().equals("Bad") || health().equals("Okay")) {
-			return "Unhappy \t";
+			return "Unhappy";
 		}
 		if (health().equals("critical oil level reached") || health().equals("really really needs oil")
 				|| (health().equals("really needs oil"))) {
 			return "Unhappy \t";
 		}
-		return "Happy \t";
+		return "Happy";
 	}
 
 	public void tick() {
-		int tickAmnt = 1; 
-		health += tickAmnt; 
-		happiness += tickAmnt;
 	}
 
 	public void play(int amountToPlay) {
